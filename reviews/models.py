9286406582744c35d474905bda7c7f5f19de3912
github-main-user <kolya.skripn@mail.ru>
2025-08-05 14:58:07 +0300

    feat: create review models

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from ads.models import Ad
+
+User = get_user_model()
+
+
+class Review(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name="reviews")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+        indexes = [
+            models.Index(fields=["author"]),
+            models.Index(fields=["ad"]),
+            models.Index(fields=["created_at"]),
+        ]
+
+    def __str__(self) -> str:
+        return f"Review by {self.author} on Ad #{self.ad_id}"
