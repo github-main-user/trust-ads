90a24b4250c9e41596f53e7aa559dfba275fe7a4
github-main-user <kolya.skripn@mail.ru>
2025-08-05 11:52:13 +0300

    feat: create model for storing ads

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ads")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["author"]),
+            models.Index(fields=["created_at"]),
+        ]
+
+    def __str__(self):
+        return f"{self.title} — {self.price}₽"
